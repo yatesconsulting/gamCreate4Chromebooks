39f00ecd83b00a1e56098482b7738bb0b7653670
yatesconsulting <github@yatesconsulting.com>
2021-06-08 11:55:16 -0600

    modularized better
    still overuses global variables, but works well.  Now only searches relative *SCH*.xls* files vs all of them

--- gamCreate.py
+++ gamCreate.py
@@ -6,20 +6,6 @@ import os #  for pipe and environ
 import csv # for processing output of gam print cros...
 import subprocess # for running gam commands
 
-codesfilename = "codes.xlsx"
-school2ou = {}
-school2notes = {}
-school2email = {}
-school2location = {}
-school2destiny = {}
-s2dcsv = ['SCHOOL,BARCODE,CF1:NAME,CF2:NOTES']
-gamexe = "gam"
-
-# from Connie's file(s), s/n to tag# and school
-# edit (14,44) if you use a different range of rows, around line  117
-sntotag = {}
-sntoschool = {}
-cmd = []
 
 def doesthisouexistingoogleadmin(ou):
 	with os.popen('{} info org "{}" nousers'.format(gamexe, ou)) as pipe:
@@ -34,6 +20,7 @@ def gamcreatecommand(ou):
 	return '{} create org "{}"'.format(gamexe, ou)
 
 def createou(ou):
+	# print(".{} cmd: {}".format(ou, cmd))
 	# return a list of OU create commands for this ou and any missing base OUs (recursivly)
 	# sloppy error routines, exit program on fail
 	# - if creating OU has no /, then it's at the top level, and probably NOT right
@@ -62,52 +49,58 @@ def createou(ou):
 			print("ERROR, no new root OU created {}, I just won't do it, I just can't bring myself to belive this is correct.  If it is, please create the root manually at admin.google.com, then try again.".format(ou))
 			exit()
 
-# work from current directory, open each excel file
-for f in glob.glob("*.xls*"):
-	# skip any temp files
-	if f[0] == "~":
-		continue
-	if f == codesfilename:
-		print ("Gathering which school(s) to process, from {}. Error 404's are OK.".format(f))
-		wb = load_workbook(f, read_only=True, data_only=True)
-		ws1 = wb.active
-		# loop over codes.xlsx for school info
-		for r in range(2,50):
-			school = ws1["a{}".format(r)].value
-			targetou = ws1["b{}".format(r)].value
-			emailconf = ws1["c{}".format(r)].value
-			notes = ws1["d{}".format(r)].value
-			location = ws1["e{}".format(r)].value
-			destiny = ws1["f{}".format(r)].value
-			if (isinstance(emailconf,str) and isinstance(targetou,str) and isinstance(school,str) and school[:2].upper() != "EX" and emailconf[:3].upper() != "NOT"):
-				school = school.strip()
-				targetou = targetou.strip()
-				emailconf = emailconf.strip()
-				if (isinstance(notes,str) ):
-					school2notes[school] = notes.strip()
-				else:
-					school2notes[school] = ""
-				if (isinstance(location,str) ):
-					school2location[school] = location.strip()
-				else:
-					school2location[school] = ""
-				
-				destinypartofmessage = ""
-				if (destiny == "Yes"):
-					school2destiny[school] =  True
-					destinypartofmessage = " and added to Destiny file"
-				school2ou[school] = targetou
-				school2email[school] = emailconf
-				ougood = doesthisouexistingoogleadmin(targetou)
-				if ougood:
-					newouornot = ""
-				else:
-					newouornot = "a NEW ou "
-					# check up one (and up one...) until you find a good root, build all needed OUs
-					cmd.extend(createou(targetou))
-				print ("*** {} Chromebooks in / by {} will be processed into {}{}{}.".format(school, emailconf, newouornot, targetou, destinypartofmessage))
-		wb.close()
-	else:
+def fillglobalvarsfromcodeexcelfile(f):
+	# see if there is anything to do, get emails from codesfilename
+	# schools = [] # list of school initials to process, returned
+	wb = load_workbook(f, read_only=True, data_only=True)
+	print ("Gathering which school(s) to process, from {}. Error 404's are OK.".format(f))
+	wb = load_workbook(f, read_only=True, data_only=True)
+	ws1 = wb.active
+	# loop over codes.xlsx for school info
+	for r in range(2,50):
+		school = ws1["a{}".format(r)].value
+		targetou = ws1["b{}".format(r)].value
+		emailconf = ws1["c{}".format(r)].value
+		notes = ws1["d{}".format(r)].value
+		location = ws1["e{}".format(r)].value
+		destiny = ws1["f{}".format(r)].value
+		if (isinstance(emailconf,str) and isinstance(targetou,str) and isinstance(school,str) and school[:2].upper() != "EX" and emailconf[:3].upper() != "NOT"):
+			school = school.strip()
+			# schools.append(school)
+			targetou = targetou.strip()
+			emailconf = emailconf.strip()
+			if (isinstance(notes,str) ):
+				school2notes[school] = notes.strip()
+			else:
+				school2notes[school] = ""
+			if (isinstance(location,str) ):
+				school2location[school] = location.strip()
+			else:
+				school2location[school] = ""
+			
+			destinypartofmessage = ""
+			if (destiny == "Yes"):
+				school2destiny[school] =  True
+				destinypartofmessage = " and added to Destiny file"
+			school2ou[school] = targetou
+			school2email[school] = emailconf
+			ougood = doesthisouexistingoogleadmin(targetou)
+			if ougood:
+				newouornot = ""
+			else:
+				newouornot = "a NEW ou "
+				# check up one (and up one...) until you find a good root, build all needed OUs
+				cmd.extend(createou(targetou))
+			print ("*** {} Chromebooks in / by {} will be processed into {}{}{}.".format(school, emailconf, newouornot, targetou, destinypartofmessage))
+	wb.close()
+	# return schools
+
+def fillglobalvarsfromsearchofexcelfiles(school):
+	# work from current directory, open each excel file
+	for f in glob.glob("*{}*.xls*".format(school)):
+		# skip any temp files
+		if f[0] == "~":
+			continue
 		print ("Adding lookups for serial/tags from {}".format(f))
 		wb = load_workbook(f, read_only=True, data_only=True)
 		for s in wb.sheetnames:
@@ -121,7 +114,7 @@ for f in glob.glob("*.xls*"):
 				room = ws1["f{}".format(r)].value
 				fulltag = "{}CB-{}".format(school,tag)
 				if (isinstance(serial,str) and serial > "" and serial not in ("No Number","None","Building","BLDG") and isinstance(school,str)): 
-					# ERROR, dup serial on line 13: BuildingCB-Tag # <> BLDGCB-Tag #
+					# ERROR, dup serial on line 13: BuildingCB-Tag # <> BLDGCB-Tag # FIX TODO
 
 					serial = serial.strip()
 					school = school.strip()
@@ -135,74 +128,93 @@ for f in glob.glob("*.xls*"):
 						sntotag[serial] = fulltag
 						sntoschool[serial] = school
 		wb.close()
-	# ~ for a in sntotag.keys():
-		# ~ print ("key {} = {}".format(a,sntotag[a]))
+		# ~ for a in sntotag.keys():
+			# ~ print ("key {} = {}".format(a,sntotag[a]))
 
-# if I do need to create new OUs, I only need to do each base once, remove dups
-cmd = sorted(list(set(cmd)))
+def gamcroscheck():
+	# ~ Now loop over all the OU / CrOS devices and add them to the list, if appropriate
+	with os.popen('{} print cros limit_to_ou / fields deviceId,serialNumber,status,lastSync,annotatedUser,annotatedLocation,annotatedAssetId,lastEnrollmentTime,orgUnitPath,notes'.format(gamexe)) as pipe:
+		reader = csv.DictReader(pipe)
+		for row in reader:
+			# ~ if this chromebook sn -> connie's file for school and tag -> codes for owners email
+			# ~ If it is a good one to register, do it
+			sn = row['serialNumber']
+			cbuser = row['annotatedUser']
+			status = row['status']
+			if (status == "ACTIVE" and sn in sntoschool.keys() and sntoschool[sn] in school2email.keys() and school2email[sntoschool[sn]] == cbuser):
+				school = sntoschool[sn]
+				deviceid = row['deviceId']
+				loc = school2location[school]
+				mname = sntotag[sn]
+				notes = school2notes[school]
+				destou = school2ou[school]		
+				cmd.append('{} update cros query:id:{} notes "{}" ou "{}" assetid "{}" location "{}"'.format(gamexe,sn,notes,destou,mname,loc))
+				# print(cmd[-1])
+				if (school2destiny[school]):
+					# add to destiny CSV also
+					s2dcsv.append("{},{},{},{}".format(school,sn,mname,notes))
 
-# ~ print("Ok, let's look at those s/n tags I found: {}".format(sntotag))
-# ~ exit()
+def warnthenruncmd():
+	print("--- Here's what I'm really going to do if you say yes: ---")
+	for a in cmd:
+		print (a)
 
-# ~ Now loop over all the OU / CrOS devices and add them to the list, if appropriate
-with os.popen('{} print cros limit_to_ou / fields deviceId,serialNumber,status,lastSync,annotatedUser,annotatedLocation,annotatedAssetId,lastEnrollmentTime,orgUnitPath,notes'.format(gamexe)) as pipe:
-	reader = csv.DictReader(pipe)
-	for row in reader:
-		# ~ if this chromebook sn -> connie's file for school and tag -> codes for owners email
-		# ~ If it is a good one to register, do it
-		sn = row['serialNumber']
-		cbuser = row['annotatedUser']
-		status = row['status']
-		if (status == "ACTIVE" and sn in sntoschool.keys() and sntoschool[sn] in school2email.keys() and school2email[sntoschool[sn]] == cbuser):
-			school = sntoschool[sn]
-			deviceid = row['deviceId']
-			loc = school2location[school]
-			mname = sntotag[sn]
-			notes = school2notes[school]
-			destou = school2ou[school]		
-			cmd.append('{} update cros query:id:{} notes "{}" ou "{}" assetid "{}" location "{}"'.format(gamexe,sn,notes,destou,mname,loc))
-			# print(cmd[-1])
-			if (school2destiny[school]):
-				# add to destiny CSV also
-				s2dcsv.append("{},{},{},{}".format(school,sn,mname,notes))
-
-print("--- Here's what I'm really going to do if you say yes: ---")
-for a in cmd:
-	print (a)
-
-l = len(cmd)
-if l > 0:
-	sifplural = ""
-	if l > 1:
-		sifplural = "s"
-	doit = input("You sure you want to run {} command{} [Y,n]? ".format(len(cmd),sifplural))
-	if (doit.strip() == "" or doit.lower().strip()[0] == "y"):
-		print ("Ok, this will take a few minutes...")
-		for c in cmd:
-			# execute c and display any errors to screen
-			print(c)
-			r = subprocess.run(c, capture_output=True)
-			if (r.stderr):
-				print ("  ERROR -- {}".format(r.stderr))
-		# create Destiny CSV if s2dcsv > 1 value
-		if len(s2dcsv) > 1:
-			user = os.environ['USERNAME']
-			filename = "{}DestinyChromebooksFile.csv".format(user)
-			# drop header line if appending to the file
-			if os.path.exists(filename):
-				del s2dcsv[0]
-				destinyfileaction = 'appended'
-			else:
-				destinyfileaction = 'created'
-			f = open(filename, "a")  
-			for l in s2dcsv:
-				f.write("{}\n".format(l))
-			print ("filename {} {} for Destiny.".format(filename, destinyfileaction))
-			f.close()
+	l = len(cmd)
+	if l > 0:
+		sifplural = ""
+		if l > 1:
+			sifplural = "s"
+		doit = input("You sure you want to run {} command{} [Y,n]? ".format(len(cmd),sifplural))
+		if (doit.strip() == "" or doit.lower().strip()[0] == "y"):
+			print ("Ok, this will take a few minutes...")
+			for c in cmd:
+				# execute c and display any errors to screen
+				print(c)
+				r = subprocess.run(c, capture_output=True)
+				if (r.stderr):
+					print ("  ERROR -- {}".format(r.stderr))
+			# create Destiny CSV if s2dcsv > 1 value
+			if len(s2dcsv) > 1:
+				user = os.environ['USERNAME']
+				filename = "{}DestinyChromebooksFile.csv".format(user)
+				# drop header line if appending to the file
+				if os.path.exists(filename):
+					del s2dcsv[0]
+					destinyfileaction = 'appended'
+				else:
+					destinyfileaction = 'created'
+				f = open(filename, "a")  
+				for l in s2dcsv:
+					f.write("{}\n".format(l))
+				print ("filename {} {} for Destiny.".format(filename, destinyfileaction))
+				f.close()
+		else:
+			print("Maybe next time, thanks.")
 	else:
-		print("Maybe next time, thanks.")
-else:
-	print("Sorry, but I couldn't find anything to do.")
+		print("Sorry, but I couldn't find anything to do.")
 
+if __name__ == "__main__":
+	codesfilename = "codes.xlsx"
+	school2ou = {}
+	school2notes = {}
+	school2email = {}
+	school2location = {}
+	school2destiny = {}
+	s2dcsv = ['SCHOOL,BARCODE,CF1:NAME,CF2:NOTES']
+	gamexe = "gam"
 
-
+	# from Connie's file(s), s/n to tag# and school
+	# edit (14,44) if you use a different range of rows, around line  117
+	sntotag = {}
+	sntoschool = {}
+	cmd = []
+	fillglobalvarsfromcodeexcelfile(codesfilename)
+	for s in school2ou.keys():
+		if s in ['PEC','PES']:
+			fillglobalvarsfromsearchofexcelfiles('pe[cs]')
+		else:
+			fillglobalvarsfromsearchofexcelfiles(s)
+	
+	cmd = sorted(list(set(cmd)))
+	gamcroscheck()
+	warnthenruncmd()
